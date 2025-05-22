5c035abe7aa1ca36bf581163aa47a8ae7ec45567
starkman <ant.stark@hotmail.com>
2025-05-22 19:08:35 +0200

    Added Error handling

--- Vattenniva_skagern.py
+++ Vattenniva_skagern.py
@@ -2,28 +2,35 @@ from flask import Flask, jsonify
 import requests
 from bs4 import BeautifulSoup
 import re
+import traceback
 
 app = Flask(__name__)
 
 def fetch_water_level():
-    url = "https://www.fortum.com/se/om-oss/energiproduktion/vattenkraft/vattenkraftverk/gullspangsalven/gullspangs-kraftverk"
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'html.parser')
-    text = soup.get_text()
+    try:
+        url = "https://www.fortum.com/se/om-oss/energiproduktion/vattenkraft/vattenkraftverk/gullspangsalven/gullspangs-kraftverk"
+        response = requests.get(url)
+        soup = BeautifulSoup(response.text, 'html.parser')
+        text = soup.get_text()
 
-    match = re.search(r'Vattennivå:\s*\+([\d.,]+)\s*m', text)
-    if match:
-        return f"+{match.group(1)} m"
-    else:
-        return None
+        match = re.search(r'Vattennivare:\s*\+([\d.,]+)\s*m', text)
+        if match:
+            return f"+{match.group(1)} m"
+        else:
+            return None
+    except Exception:
+            print(f"Error: {traceback.format_exc()}")
     
 @app.route('/api/vattenniva')
 def get_vattenniva():
-    level = fetch_water_level()
-    if level:
-        return jsonify({'vattenniva': level})
-    else:
-        return jsonify({'error': 'Water level not found'}), 404
-    
+    try:
+        level = fetch_water_level()
+        if level:
+            return jsonify({'vattenniva': level})
+        else:
+            return jsonify({'error': 'Water level not found'}), 404
+    except Exception:
+            print(f"Error: {traceback.format_exc()}")
+        
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="192.168.50.50", port="9500", debug=False)
